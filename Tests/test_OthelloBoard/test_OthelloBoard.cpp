39edc747c6596d8cf0088774095f441b61337086
Phantom <138377375+RealPhant0m@users.noreply.github.com>
2026-08-06 19:11:18 +0330

    test: write test for OthelloBoard class

---
+++ Tests/test_OthelloBoard/test_OthelloBoard.cpp
@@ -0,0 +1,71 @@
+#include "../../include/OthelloBoard.hpp"
+#include <cassert>
+#include <iostream>
+
+using namespace othello;
+
+void testInitialBoardState() {
+    OthelloBoard board;
+    auto boardString = board.convertBoardFormat();
+    assert(boardString.size() == BOARD_SIZE * BOARD_SIZE);
+
+    for (int row = 0; row < BOARD_SIZE; ++row) {
+        for (int col = 0; col < BOARD_SIZE; ++col) {
+            assert(board.board()[row][col] == OthelloBoard::toChar(Player::Empty));
+        }
+    }
+}
+
+void testConvertBoardFormatRoundTrip() {
+    std::string boardString(BOARD_SIZE * BOARD_SIZE, '0');
+    boardString[3 * BOARD_SIZE + 3] = '1';
+    boardString[3 * BOARD_SIZE + 4] = '2';
+    boardString[4 * BOARD_SIZE + 3] = '2';
+    boardString[4 * BOARD_SIZE + 4] = '1';
+
+    OthelloBoard board(boardString);
+    assert(board.convertBoardFormat() == boardString);
+}
+
+void testGetValidMovesEmptyBoard() {
+    OthelloBoard board;
+    auto validMoves = board.getValidMoves(Player::Black);
+    assert(validMoves.empty());
+}
+
+void testApplyMoveFlipsDiscs() {
+    std::string boardString(BOARD_SIZE * BOARD_SIZE, '0');
+    boardString[3 * BOARD_SIZE + 3] = '1';
+    boardString[3 * BOARD_SIZE + 4] = '2';
+    boardString[4 * BOARD_SIZE + 3] = '2';
+    boardString[4 * BOARD_SIZE + 4] = '1';
+
+    OthelloBoard board(boardString);
+    auto validMoves = board.getValidMoves(Player::Black);
+    assert(!validMoves.empty());
+
+    bool foundMove = false;
+    Move chosenMove;
+    for (auto& move : validMoves) {
+        if (move.row == 2 && move.col == 4) {
+            foundMove = true;
+            chosenMove = move;
+            break;
+        }
+    }
+    assert(foundMove);
+
+    Board nextBoard = board.applyMove(chosenMove, Player::Black);
+    assert(nextBoard[2][4] == OthelloBoard::toChar(Player::Black));
+    assert(nextBoard[3][4] == OthelloBoard::toChar(Player::Black));
+}
+
+int main() {
+    testInitialBoardState();
+    testConvertBoardFormatRoundTrip();
+    testGetValidMovesEmptyBoard();
+    testApplyMoveFlipsDiscs();
+
+    std::cout << "All OthelloBoard tests passed.\n";
+    return 0;
+}
